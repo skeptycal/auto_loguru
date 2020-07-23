c414cb45ae1e007838c4ea8a9798df7dac8799bf
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-07-23 14:28:59 -0700

    initial commit

--- tests/exceptions/source/ownership/_init.py
+++
@@ -1,6 +0,0 @@
-import os, sys, sysconfig
-
-
-usersite = os.path.abspath(os.path.join(os.path.dirname(__file__), "usersite"))
-sys.path.append(usersite)
-sysconfig._INSTALL_SCHEMES["posix_user"]["purelib"] = usersite
