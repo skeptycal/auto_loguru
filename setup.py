eef85a6064c70b40396339758fffa9f550dc45a7
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-08-06 02:06:37 -0500

    add message to use poetry

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-# from setuptools import setup, Extension
-# from typing import List
-# setup(
-#     name="autologuru",
-#     version="0.2.0",
-#     description="Fancy defaults for the awesome loguru logs!",
-#     license="MIT",
-#     long_description=open('README.md').read(),
-#     long_description_content_type="text/md",
-# )
+#!/usr/bin/env python3
+
+print("Run 'poetry' for setup information.")
+print()
+print('Normal sequence is:')
+print('  poetry install')
+print('  poetry build')
+print('  poetry publish')
